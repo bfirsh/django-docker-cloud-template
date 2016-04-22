03dee76bac526361c61ab0cda826a7a2b350cf3f
Ben Firshman <ben@firshman.co.uk>
2016-04-22 15:55:43 -0700

    Only use whitenoise static files in production
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- project_name/settings.py
+++ project_name/settings.py
@@ -135,7 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "{{ project_name }}/static"),
 ]
